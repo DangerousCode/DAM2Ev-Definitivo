75bf05c2376cc6cd3535d1c30a260beac0df6f60
DangerousCode <asantosq@hotmail.es>
2016-03-02 12:42:24 +0100

    pepooo

--- ServiciosYProcesos/BicisServidor/src/servidor/Servidor.java
+++ ServiciosYProcesos/BicisServidor/src/servidor/Servidor.java
@@ -9,21 +9,21 @@ public class Servidor {
 
 	public static void main(String[] args) throws IOException, InterruptedException {
 		ServerSocket serverSocket=new ServerSocket(6000);
-		Bici[] bici=new Bici[20];
+		Bici[] bici=new Bici[7];
 		Semaphore semaforo=new Semaphore(1);
-		Hilo[] hilo=new Hilo[20];
+		Hilo[] hilo=new Hilo[50];
 		Ventanas ventana=new Ventanas("Server");
 		ventana.escribecadena("Servidor Creado");
-		for(int i=0;i<20;i++){
+		for(int i=0;i<7;i++){
 			bici[i]=new Bici(i+1,false);
 		}
 		
-		for(int i=0;i<20;i++){
+		for(int i=0;i<50;i++){
 			Socket cliente=serverSocket.accept();
 			hilo[i]=new Hilo(cliente,semaforo,bici,ventana);
 			hilo[i].start();
 		}
-		for(int i=0;i<20;i++){
+		for(int i=0;i<50;i++){
 			hilo[i].join();
 		}
 		serverSocket.close();
